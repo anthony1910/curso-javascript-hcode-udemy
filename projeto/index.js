593200f0a94deaf4246b6da5cf6809f3f98d2141
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2018-05-28 23:13:33 -0300

    Aula 04

---
+++ projeto/index.js
@@ -0,0 +1,56 @@
+// Aqui eu escrevo no console
+/**
+ * 
+ * comentário para documentação
+ * 
+ */
+/* exemplos de console
+console.log('Olá mundo');
+console.log('Olá mundo');
+console.log('Olá mundo');*/
+
+// variávies
+/*var olaMundo = "Hello World!";
+
+console.log(olaMundo);
+console.log(olaMundo);
+*/
+
+// operadores
+/*
+let a = 10; // atribuição
+const b = "10"; // atribuição
+
+console.log(a == b); // operador de comparação. Compara o valor
+console.log(a === b); // operador de comparação. Compara o valor e o tipo da variável
+console.log(a != b); // operador de comparação. Compara se o valor é diferente
+*/
+
+// condicionais
+/*
+let cor = "azul";
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else if(cor === "amarelo") {
+
+    console.log("atenção");
+
+} else if(cor === "vermelho") {
+
+    console.log("pare");
+
+}
+*/
+
+// tabuada
+let n = 7;
+
+for (let i = 0; i <= 10; i++) {
+
+    console.log(` ${i} X ${n} = ${i*n} `); // atual
+    // antigamente -> console.log(i + " X " + n + " = " + (i * n));
+
+}
