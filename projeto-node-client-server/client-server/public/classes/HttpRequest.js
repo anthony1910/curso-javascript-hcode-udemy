5751336db571ae8d73ee322694633102cba77b94
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2018-09-09 21:47:25 -0300

    Aula 84 85

--- projeto-node-client-server/client-server/public/classes/HttpRequest.js
+++ projeto-node-client-server/client-server/public/classes/HttpRequest.js
@@ -26,9 +26,6 @@ class HttpRequest {
 
     static request(method, url, params = {}){
 
-        console.log(method, url, params = {});
-        // return false;
-
         return new Promise((resolve, reject) => {
 
             let ajax = new XMLHttpRequest();
