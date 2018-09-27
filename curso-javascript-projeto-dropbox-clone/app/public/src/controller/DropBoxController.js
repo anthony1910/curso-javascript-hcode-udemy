5f9e3743538e707255b59716787d403eea52ba22
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2018-09-27 11:41:54 -0300

    Seção 6 - Aula 96

--- curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
+++ curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
@@ -354,6 +354,8 @@ class DropBoxController {
             <div class="name text-center">${file.name}</div>
         `;
 
+        this.initEventsLi(li);
+
         return li;
 
     }
@@ -372,6 +374,16 @@ class DropBoxController {
                 this.listFilesEl.appendChild(this.getFileView(data, key));
 
             });
+
+        });
+
+    }
+
+    initEventsLi(li){
+
+        li.addEventListener('click', e => {
+
+            li.classList.toggle('selected');
 
         });
 
