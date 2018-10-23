6aed754e57e30f9f2a77ce573b5f58bd33192db9
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2018-10-22 23:25:36 -0300

    Seção 7 - Aula 134

--- curso-javascript-projeto-whatsapp-clone/src/controller/WhatsAppController.js
+++ curso-javascript-projeto-whatsapp-clone/src/controller/WhatsAppController.js
@@ -10,10 +10,29 @@ export class WhatsAppController {
 
         console.log('WhatsAppController ok');
 
+        this._firebase = new Firebase();
+        this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
-        this._firebase = new Firebase();
+
+    }
+
+    initAuth(){
+
+        this._firebase.initAuth()
+            .then(response => {
+                
+                this._user = response.user;
+
+                this.el.appContent.css({
+                    display: 'flex'
+                });
+
+            })
+            .catch(err => {
+                console.error(err);
+            });
 
     }
 
